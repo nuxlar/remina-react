15ded04bd5fefb69e1fcd2a04e15279f63dddab6
Romy Maghsoudi <romy.mag9@gmail.com>
2021-04-06 14:07:30 -0400

    login logic

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,4 +1,11 @@
+import LoginButton from "../auth/LoginButton";
+
 function Dashboard() {
-  return <h3>Dashboard Page</h3>;
+  return (
+    <div>
+      <h3>Dashboard Page</h3>
+      <LoginButton></LoginButton>
+    </div>
+  );
 }
 export default Dashboard;
