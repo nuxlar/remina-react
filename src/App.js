03dba751ccb69c76e9b5cd51d9f725093946e93d
Romy Maghsoudi <romy.mag9@gmail.com>
2021-04-06 14:53:59 -0400

    NavBar component and implementation

--- src/App.js
+++ src/App.js
@@ -1,66 +1,34 @@
-import logo from "./logo.svg";
+import NavBar from "./components/NavBar";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Dashboard from "./components/dashboard";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
 import HabitPage from "./components/habits/HabitPage";
 import GoalPage from "./components/goals/GoalPage";
 import TodoPage from "./components/todos/TodoPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Router>
-          <div>
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/">Dashboard</Link>
-                </li>
-                <li>
-                  <Link to="/todos">Todos</Link>
-                </li>
-                <li>
-                  <Link to="/habits">Habits</Link>
-                </li>
-                <li>
-                  <Link to="/goals">Goals</Link>
-                </li>
-              </ul>
-            </nav>
-
-            {/* A <Switch> looks through its children <Route>s and
+    <Router>
+      <div className="App">
+        <NavBar />
+        {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-            <Switch>
-              <Route path="/habits">
-                <HabitPage />
-              </Route>
-              <Route path="/todos">
-                <TodoPage />
-              </Route>
-              <Route path="/goals">
-                <GoalPage />
-              </Route>
-              <Route path="/">
-                <Dashboard />
-              </Route>
-            </Switch>
-          </div>
-        </Router>
-      </header>
-    </div>
+        <Switch>
+          <Route path="/habits">
+            <HabitPage />
+          </Route>
+          <Route path="/todos">
+            <TodoPage />
+          </Route>
+          <Route path="/goals">
+            <GoalPage />
+          </Route>
+          <Route path="/">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,35 @@
+import logo from "../logo.svg";
+import { Navbar, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+function NavBar() {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand as={Link} to="/">
+          <img
+            alt=""
+            src={logo}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{" "}
+          Project Remina
+        </Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link as={Link} to="/todos">
+            Todos
+          </Nav.Link>
+          <Nav.Link as={Link} to="/habits">
+            Habits
+          </Nav.Link>
+          <Nav.Link as={Link} to="/goals">
+            Goals
+          </Nav.Link>
+        </Nav>
+      </Navbar>
+    </>
+  );
+}
+
+export default NavBar;
