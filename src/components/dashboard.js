49892a3450bb0cd85847d9ac197890ff123697db
Romy Maghsoudi <romy.mag9@gmail.com>
2021-04-06 16:04:53 -0400

    conditionally render dashboard or homepage

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -8,12 +8,20 @@ function Dashboard() {
     return <div>Loading ...</div>;
   }
   return (
-    isAuthenticated && (
-      <div>
-        <h2>Dashboard Page</h2>
-        <h3>{user.name}</h3>
-      </div>
-    )
+    <div>
+      {isAuthenticated ? (
+        <>
+          <h2>Dashboard Page</h2>
+          <h3>{user.name}</h3>
+        </>
+      ) : (
+        <>
+          {" "}
+          <h2>Home Page</h2>
+          <h3>About</h3>
+        </>
+      )}
+    </div>
   );
 }
 export default Dashboard;
