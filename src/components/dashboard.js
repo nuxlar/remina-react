a45eff890e01e5f4d7cff3af487dce5b4dfd86b0
Romy Maghsoudi <romy.mag9@gmail.com>
2021-04-08 08:36:55 -0400

    dashboard cleanup

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import Button from "react-bootstrap/Button";
 import axios from "axios";
-import { getOrCreateUser } from "../api/apiCalls";
 
 function Dashboard() {
   const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
@@ -10,11 +8,10 @@ function Dashboard() {
 
   useEffect(() => {
     const trying = async () => {
-    try {
+      try {
         const accessToken = await getAccessTokenSilently({
           audience: `https://project-remina/`,
         });
-        console.log(accessToken);
         const response = await axios.post(
           "http://localhost:8000/users",
           { user: { username: user.sub } },
@@ -24,36 +21,33 @@ function Dashboard() {
             },
           }
         );
-      setApiUser(response.data);
-    } catch (error) {
-      console.log(error.message);
-    }};
+        setApiUser(response.data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
 
-    trying()
-  }, [user])
+    trying();
+  }, [user]);
 
   if (!apiUser && isAuthenticated) {
-    return (
-      <div>
-        Loading...
-      </div>
-    );
+    return <div>Loading...</div>;
   }
 
   return (
     <div>
       {isAuthenticated && apiUser ? (
-      <>
-        <h2>Dashboard Page</h2>
-        <h3>Level {apiUser.level}</h3>
-      </>
+        <>
+          <h2>Dashboard Page</h2>
+          <h3>Level {apiUser.level}</h3>
+        </>
       ) : (
         <>
           {" "}
           <h2>Home Page</h2>
           <h3>About</h3>
-        </> 
-       )} 
+        </>
+      )}
     </div>
   );
 }
