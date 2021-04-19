bb99fbd119d7927c17ca1c89c38ec00985bb04e2
Romy Maghsoudi <romy.mag9@gmail.com>
2021-04-19 08:25:22 -0400

    creating homepage

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,20 @@
 import React, { useEffect, useState } from "react";
-import { Spinner, Container, Col, Row, ProgressBar } from "react-bootstrap";
+import {
+  Spinner,
+  Container,
+  Col,
+  Row,
+  ProgressBar,
+  Jumbotron,
+  CardDeck,
+  Card,
+} from "react-bootstrap";
 import { useAuth0 } from "@auth0/auth0-react";
 import axios from "axios";
 import GoalPage from "./goals/GoalPage";
 import HabitPage from "./habits/HabitPage";
 import TodoPage from "./todos/TodoPage";
+import LoginButton from "../auth/LoginButton";
 
 function Dashboard() {
   const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
@@ -55,10 +65,10 @@ function Dashboard() {
 
   if (!apiUser && isAuthenticated) {
     return (
-      <Container fluid className="spinnerContainer">
-        <Col className="justify-content-md-center">
+      <Container fluid classNameName="spinnerContainer">
+        <Col classNameName="justify-content-md-center">
           <Spinner animation="border" role="status">
-            <span className="sr-only">Loading...</span>
+            <span classNameName="sr-only">Loading...</span>
           </Spinner>
         </Col>
       </Container>
@@ -66,15 +76,15 @@ function Dashboard() {
   }
 
   return (
-    <div>
+    <>
       {isAuthenticated && apiUser ? (
         <Container>
-          <Row className="justify-content-md-center">
+          <Row classNameName="justify-content-md-center">
             <Col>
               <h2>Your Dashboard</h2>
             </Col>
           </Row>
-          <Row className="justify-content-md-center">
+          <Row classNameName="justify-content-md-center">
             <Col>
               <h3>
                 Level {apiUser.level} | {apiUser.xp}/{apiUser.xp_to_lvlup}XP
@@ -86,26 +96,84 @@ function Dashboard() {
               />
             </Col>
           </Row>
-          <hr className="solid" />
+          <hr classNameName="solid" />
           <Row>
             <GoalPage apiUser={apiUser} getApiUser={getApiUser} />
           </Row>
-          <hr className="solid" />
+          <hr classNameName="solid" />
           <Row>
             <HabitPage apiUser={apiUser} getApiUser={getApiUser} />
           </Row>
-          <hr className="solid" />
+          <hr classNameName="solid" />
           <Row>
             <TodoPage apiUser={apiUser} getApiUser={getApiUser} />
           </Row>
         </Container>
       ) : (
         <>
-          {" "}
-          <h2>Home Page (Login/Sign Up to get started!)</h2>
+          <Jumbotron fluid className="home-jumbotron">
+            <div className="jumbotron-content">
+              <h1>Project Remina</h1>
+              <p>An app made to level up your productivity</p>
+              <LoginButton />
+            </div>
+          </Jumbotron>
+          <CardDeck>
+            <Card style={{ height: "300px" }}>
+              <Card.Body>
+                <Card.Title>Instant feedback loop</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">
+                  Why was it made?
+                </Card.Subtitle>
+                <Card.Text>
+                  I've always wished there could be a real-life leveling system
+                  and that's what project remina is for productivity. By
+                  receiving XP and leveling up for doing things you'd normally
+                  do, you have a tangible, fun way of making progress.
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <small className="text-muted">{"Made with love <3"}</small>
+              </Card.Footer>
+            </Card>
+            <Card>
+              <Card.Body>
+                <Card.Title>Goals, Habits, To-dos</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">
+                  How productivity is tracked
+                </Card.Subtitle>
+                <Card.Text>
+                  These are 3 ways project remina helps level up your
+                  productivity. You're able to create a goal for today, this
+                  week, and this month, track habits for the week, and manage
+                  good ol' to-dos.
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <small className="text-muted">Pillars of productivity</small>
+              </Card.Footer>
+            </Card>
+            <Card>
+              <Card.Body>
+                <Card.Title>What it's made with</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">
+                  Fun tech stuff
+                </Card.Subtitle>
+                <Card.Text>
+                  The frontend is built with React (Javascript ES6) while the
+                  API is written in Django (Python 3) that uses a Postgres
+                  database. Authentication is performed using a 3rd party app
+                  called Auth0.
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <small className="text-muted">Beep boop</small>
+              </Card.Footer>
+            </Card>
+          </CardDeck>
         </>
       )}
-    </div>
+    </>
   );
 }
 export default Dashboard;
